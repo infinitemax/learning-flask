1796f37f068ebf12508bf0767ee26312fcd9a9f9
infinitemax <m.brookman.byrne@gmail.com>
2023-12-01 17:09:50 +0000

    finish CRUD operations

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,11 @@
-from flask.templating import render_template_string
-from werkzeug.datastructures import RequestCacheControl
+import sys
 from werkzeug.utils import redirect
 from application import app
-from flask import render_template, request, flash
+from flask import render_template, request, flash, url_for
 from .forms import TodoForm
 from application import db
 from datetime import datetime
+from bson import ObjectId
 
 # rendering an html file from the templates folder
 @app.route("/")
@@ -39,5 +39,40 @@ def add_todo():
     
     else: 
         form = TodoForm()
-        
     return render_template("add_todo.html", form = form)
+
+# update the item
+@app.route("/update_todo/<id>", methods = ["POST", "GET"])
+def update_todo(id):
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_description = form.description.data
+        completed = form.completed.data
+
+        db.todo_flask.find_one_and_update({"_id": ObjectId(id)}, {"$set": {
+            "name" : todo_name,
+            "description" : todo_description,
+            "completed" : completed,
+            "date_created" : datetime.utcnow()
+        }})
+
+        flash("todo updated", "success")
+        return redirect("/")
+
+    # this bit renders the prefilled form
+    else:
+        form = TodoForm()
+
+        todo = db.todo_flask.find_one_or_404({"_id" : ObjectId(id)})
+        form.name.data = todo.get("name", None)
+        form.description.data = todo.get("description", None)
+        form.completed.data = todo.get("completed", None)
+    
+    return render_template("add_todo.html", form = form)
+
+@app.route("/delete_todo/<id>")
+def delete_todo(id):
+    db.todo_flask.find_one_and_delete({"_id": ObjectId(id)})
+    flash("item deleted")
+    return redirect("/")
